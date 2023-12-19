c5dfd708da9ec6cfd4db2434f9f813480a178128
HugoVerneuil <154213578+HugoVerneuil@users.noreply.github.com>
2023-12-19 10:36:53 +0100

    Module IPV4 : Tableau des sous réseaux dynamique + formulaire taille des sous réseaux

--- src/Module_IPV4.php
+++ src/Module_IPV4.php
@@ -13,11 +13,33 @@ else{
 }
 ?>
 <form action="./Module_IPV4.php" method="post" name="formul_nb_res" id="formul_nb_res">
-Nombre de sous réseaux souhaités : <input type="number" name="nb_res" value="<?php echo $nb_res ?>">  <input type="submit" value="Valider" name="ok_nb_res">
+    Nombre de sous réseaux souhaités : <input type="number" name="nb_res" value="<?php echo $nb_res ?>">  <input type="submit" value="Valider" name="ok_nb_res">
 </form>
 <br>
 <form action="./actionModule_IPV4.php" method="post" name="formul_sous_res" onsubmit="return valider()" id="formul_sous_res">
     Adresse IPV4 : <input type="text" name="addr" required pattern="^((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])$"> / <input type="number" name="mask" min="8" max="30" required>
+    <br>
+    <br>
+    <?php
+    echo "<table>";
+    echo "<tr><th><p>Sous réseaux</p></th><th><p>Taille</p></th></tr>";
+    $res = 1;
+    while($res < $nb_res+1){
+        $taille_res = 'taille_res_'.$res;
+        echo "<tr>";
+        echo "<td>$res</td>";
+        echo "<td><input type='number' name=$taille_res min='1' required> </td>";
+        echo "</tr>";
+        $res++;
+    }
+    echo "</table>
+<input type='hidden' name='nb_res' value=$nb_res>
+";
+
+    ?>
+    <br>
+    <br>
+    <input type="submit" value="Calculer" name="ok_calc">
 </form>
 </body>
 </html>
