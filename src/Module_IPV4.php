7968424dd41fbc2a08d5c92df3b798dd6006bb79
HugoVerneuil <154213578+HugoVerneuil@users.noreply.github.com>
2023-12-19 10:32:36 +0100

    Module IPV4 : Formulaire adresse IPV4 + masque

--- src/Module_IPV4.php
+++ src/Module_IPV4.php
@@ -16,5 +16,8 @@ else{
 Nombre de sous réseaux souhaités : <input type="number" name="nb_res" value="<?php echo $nb_res ?>">  <input type="submit" value="Valider" name="ok_nb_res">
 </form>
 <br>
+<form action="./actionModule_IPV4.php" method="post" name="formul_sous_res" onsubmit="return valider()" id="formul_sous_res">
+    Adresse IPV4 : <input type="text" name="addr" required pattern="^((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])$"> / <input type="number" name="mask" min="8" max="30" required>
+</form>
 </body>
 </html>
