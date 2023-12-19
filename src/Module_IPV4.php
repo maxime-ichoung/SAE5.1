a529deb276b35b2edd27ec3c2fcfb219ddb5179a
HugoVerneuil <154213578+HugoVerneuil@users.noreply.github.com>
2023-12-19 10:29:34 +0100

    Module IPV4 : Formulaire choix du nombre de sous réseaux

--- src/Module_IPV4.php
+++ src/Module_IPV4.php
@@ -4,5 +4,17 @@
     <title>Module IPV4</title>
 </head>
 <body>
+<?php
+if(isset($_POST['nb_res'])){
+    $nb_res = $_POST['nb_res'];
+}
+else{
+    $nb_res = 6;
+}
+?>
+<form action="./Module_IPV4.php" method="post" name="formul_nb_res" id="formul_nb_res">
+Nombre de sous réseaux souhaités : <input type="number" name="nb_res" value="<?php echo $nb_res ?>">  <input type="submit" value="Valider" name="ok_nb_res">
+</form>
+<br>
 </body>
 </html>
