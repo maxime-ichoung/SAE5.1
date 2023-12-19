c644bf23dc8190bd7fcebc724fbea33e04706d67
HugoVerneuil <154213578+HugoVerneuil@users.noreply.github.com>
2023-12-19 10:43:33 +0100

    Module IPV4 : tableau dynamique des sous réseaux

--- src/actionModule_IPV4.php
+++ src/actionModule_IPV4.php
@@ -4,9 +4,22 @@ if(isset($_POST['ok_calc'], $_POST['nb_res'], $_POST['addr'], $_POST['mask'])){
     $addr = $_POST['addr'];
     $mask = $_POST['mask'];
     $nb_hosts = (2**(32-$mask))-2;
+    echo $nb_hosts;
 }
-?>
 
+
+echo "<table>";
+echo "<tr><th><p>Sous réseaux</p></th><th><p>Taille souhaitée</p></th><th><p>Taille allouée</p></th><th><p>Adresse</p></th><th><p>Masque</p></th><th><p>Ensemble d'adresse attribuable</p></th><th><p>Broadcast</p></th></tr>";
+$res = 1;
+while($res < $nb_res+1){
+    $taille_res = 'taille_res_'.$res;
+    echo "<tr>";
+    echo "<td>$res</td>";
+    echo "<td>"; echo $_POST[$taille_res]; echo"</td>";
+    echo "</tr>";
+    $res++;
+}
+echo "</table>";
 
 
 
