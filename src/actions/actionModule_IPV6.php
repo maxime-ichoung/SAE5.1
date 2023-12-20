671d3f50d3ecf739fdce931c67f3d12191105b62
Raphaël <raphael.dimascio.pro@gmail.com>
2023-12-20 09:15:46 +0100

    Simplification ipv6 et calcul d'hexa en binaire pour l'octet de poids fort ipv6

--- src/actions/actionModule_IPV6.php
+++ src/actions/actionModule_IPV6.php
@@ -17,3 +17,31 @@ for ($i = 0; $i < sizeof($parse_ipv6) - 1; $i++){
 foreach ($parse_ipv6 as $part){
     echo $part . ":";
 }
+
+function ipv6_simplifiee($address){
+    return inet_ntop(inet_pton($address));
+}
+
+function binaire_poids_fort($address) {
+    $packed_address = inet_pton($address);
+    $hex_str = bin2hex($packed_address);
+    $binary_str = '';
+    $hex_str_result = '';
+
+    for ($i = 0; $i < 4; $i++) {
+        $byte = substr($hex_str, $i*2, 2);
+        $binary_byte = base_convert($byte, 16, 2);
+        $binary_byte_padded = str_pad($binary_byte, 8, '0', STR_PAD_LEFT);
+        $binary_str .= $binary_byte_padded . ' ';
+        $hex_str_result .= strtoupper($byte) . ' ';
+    }
+
+    return array('binaire' => trim($binary_str), 'hexa' => trim($hex_str_result));
+}
+
+$result = binaire_poids_fort($ipv6);
+echo '<br>Adresse IPv6 simplifiée : ' . ipv6_simplifiee($ipv6);
+echo '<br>Affichage binaire et hexadécimal des deux octets les plus significatifs : ';
+for ($i = 0; $i < 4; $i++) {
+    echo '<br>Hexa: ' . substr($result['hexa'], $i*3, 2) . ', Binaire: ' . substr($result['binaire'], $i*9, 8);
+}
