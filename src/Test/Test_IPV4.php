9c84f4de1b64de07c1755d1c4c3ef751ba3e91c0
HugoVerneuil <154213578+HugoVerneuil@users.noreply.github.com>
2023-12-22 12:39:19 +0100

    Mise en place des fonctions phpUnit de test du module

--- src/Test/Test_IPV4.php
+++ src/Test/Test_IPV4.php
@@ -5,5 +5,15 @@ use PHPUnit\Framework\TestCase;
 
 class TestIPV4 extends TestCase
 {
+    public function Test_IPV4_taille_mask_sous_res(){
 
+    }
+
+    public function Test_IPV4_mask_cdri_vers_dec(){
+
+    }
+
+    public function Test_IPV4_addr_broad_plage_sous_res(){
+
+    }
 }
